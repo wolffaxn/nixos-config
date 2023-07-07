e637721bde2bb85b949c3b671085e5794a55a416
Alexander Wolff <wolffaxn@gmail.com>
2023-07-07 14:15:35 +0200

    feat(git): initial conf

---
+++ modules/git/default.nix
@@ -0,0 +1,11 @@
+{
+    programs.git = {
+        enable = true;
+        userName = "Alexander Wolff";
+        userEmail = "wolffaxn@gmail.com";
+        signing = {
+            key = "CC475A552C299D3B";
+            signByDefault = true;
+        };
+    };
+}

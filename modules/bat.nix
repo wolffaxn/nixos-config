9637789e6ac9da580e6be451965702f8c7d7f374
Alexander Wolff <wolffaxn@gmail.com>
2023-07-29 23:57:38 +0200

    feat(bat): catppuccin theme

--- modules/bat.nix
+++ modules/bat.nix
@@ -1,8 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.bat = {
     enable = true;
     config = {
-      theme = "base16";
+      theme = "Catppuccin-mocha";
     };
+  };
+
+  xdg.configFile = {
+    "bat/themes/Catppuccin-mocha.tmTheme".text =  builtins.readFile (pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
+      sha256 = "a8c40d2466489a68ebab3cbb222124639221bcf00c669ab45bab242cbb2783fc";
+    });
   };
 }

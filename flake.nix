5c68e0baf77e25999fb8e5b5ddccd0824e7d3ac0
Alexander Wolff <wolffaxn@gmail.com>
2023-08-14 15:45:46 +0200

    feat(nix): access to stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,17 +2,22 @@
   description = "wolffaxn nix system configurations";
 
   inputs = {
+    # nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
 
+    # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # nix-darwin
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ { nixpkgs, home-manager, darwin, ... }: {
 
+    # macOS systems using nix-darwin
     darwinConfigurations = {
       deimos = darwin.lib.darwinSystem {
         system = "aarch64-darwin";

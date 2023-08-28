e4ea666c3fb7bed16b9b22962943408f42afc47e
Alexander Wolff <wolffaxn@gmail.com>
2023-08-28 21:27:15 +0200

    feat(fish): shell aliases
    Signed-off-by: Alexander Wolff <wolffaxn@gmail.com>

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -1,11 +1,38 @@
 { config, lib, pkgs, ... }: {
-  programs = {
-    fish = {
-      enable = true;
-      shellInit = builtins.readFile ./init.fish;
-      interactiveShellInit = ''
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      '';
+  programs.fish = {
+    enable = true;
+    shellInit = builtins.readFile ./init.fish;
+    interactiveShellInit = ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
+
+    shellAliases = {
+      # docker
+      d = "docker";
+      
+      # git
+      g = "git";
+      ga = "git add";
+      gaa = "git add -A";
+      gb = "git branch";
+      gc = "git commit";
+      gcm = "git commit -sm";
+      gco = "git checkout";
+      gcp = "git cherry-pick";
+      gdiff = "git diff";
+      gp = "git push origin HEAD";
+      gs = "git status";
+      gsb = "git switch";
+      gsm = "git switch (git main-branch)";
+      gt = "git tag";
+
+      # neovim
+      v = "nvim";
+      vi = "nvim";
+      vim = "nvim";
+
+      # terraform
+      tf = "terraform";
     };
   };
 

3d45150b29d05c39c57525bd5972292a759fe787
Alexander Wolff <wolffaxn@gmail.com>
2023-10-22 21:06:21 +0200

    feat(nix): add font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,12 +3,15 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
-    (nerdfonts.override {
-      fonts = [
-        "FiraCode"
-        "Hack"
-        "JetBrainsMono"
-      ];
-    })
+    # monospace font with programming ligatures
+    fira-code
+    # monospace font for Firefox OS
+    fira-mono
+    # a typeface designed for source code
+    hack-font
+    # a typeface made for developers
+    jetbrains-mono
+    # a font for programming and code review
+    mononoki    
   ];
 }

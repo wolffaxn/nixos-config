b143fdcee3c4d78836acc279b19e139106f3ba11
Alexander Wolff <wolffaxn@gmail.com>
2023-10-30 21:22:14 +0100

    feat(nix): don't use nix-shell

--- nixpkgs.nix
+++
@@ -1,6 +0,0 @@
-let nixpkgs = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
-in
-import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs.rev}.tar.gz";
-  sha256 = nixpkgs.narHash;
-})
--- shell.nix
+++
@@ -1,8 +0,0 @@
-# shell with flake-enabled nix and home-manager
-
-{ pkgs ? (import ./nixpkgs.nix){} }: {
-  default = pkgs.mkShell {
-    NIX_CONFIG = "experimental-features = nix-command flakes";
-    nativeBuildInputs = with pkgs; [ nix home-manager git gnupg ];
-  };
-}

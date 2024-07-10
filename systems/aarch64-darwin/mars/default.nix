6544c5332a3127f9d20c2083b1c1ed5a94cd2240
Alexander Wolff <wolffaxn@gmail.com>
2024-07-10 23:12:08 +0200

    flake.lock: Update

---
+++ systems/aarch64-darwin/mars/default.nix
@@ -0,0 +1,40 @@
+{ inputs, lib, pkgs, ... }: {
+
+  environment.systemPath = [ "/opt/homebrew/bin" ];
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  networking = {
+    computerName = "Mac Mini";
+    hostName = "mars";
+    localHostName = "mars";
+
+    # configure DNS for WiFi
+    knownNetworkServices = ["Wi-Fi"];
+    # use cloudflare
+    dns = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+  };
+
+  # auto upgrade nix package and the daemon service
+  services.nix-daemon.enable = true;
+
+  # keyboard
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+
+  time.timeZone = "Europe/Berlin";
+
+  # we need to set this up so Nix knows what our home directory is (https://github.com/nix-community/home-manager/issues/4026)
+  users.users."alex" = {
+    name = "alex";
+    home = "/Users/alex";
+    shell = pkgs.fish;
+  };
+
+  system.stateVersion = 4;
+}

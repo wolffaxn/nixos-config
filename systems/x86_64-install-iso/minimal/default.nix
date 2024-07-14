e6be4002c1e0d9a03a0a0f48e81dce81319c2758
Alexander Wolff <wolffaxn@gmail.com>
2024-07-14 17:37:15 +0200

    feat(nix): add snowfall flake

---
+++ systems/x86_64-install-iso/minimal/default.nix
@@ -0,0 +1,23 @@
+{lib, ...}: {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.wireless.enable = lib.mkForce false;
+  networking.networkmanager.enable = true;
+
+  nix.enable = true;
+  services = {
+    openssh.enable = true;
+  };
+
+  system = {
+    locale.enable = true;
+  };
+
+  user = {
+    name = "nixos";
+    initialPassword = "nixos";
+  };
+
+  system.stateVersion = "24.05";
+}

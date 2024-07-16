2fa99f5efc5e4319a31fe078acc8e5d843fde52c
Alexander Wolff <wolffaxn@gmail.com>
2024-07-16 21:17:42 +0200

    feat(nix): add nix shell

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -1,18 +1,12 @@
 { inputs, pkgs, ... }:
+
   pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes";
 
     packages = with pkgs; [
-      age
-      alejandra
-      deadnix
-      deploy-rs
       git
       gnupg
-      home-manager
-      inputs.nixos-anywhere.packages.${pkgs.system}.nixos-anywhere
+      openssh
       sops
-      ssh-to-age
-      statix
     ];
 }

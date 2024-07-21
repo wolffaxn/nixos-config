41806273666065c884fa02fc11ffeb5fbcde3043
Alexander Wolff <wolffaxn@gmail.com>
2024-07-21 18:05:36 +0200

    feat(hm): add nix-index-database

--- flake.nix
+++ flake.nix
@@ -21,6 +21,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixos-anywhere = {
       url = "github:numtide/nixos-anywhere";
       inputs.nixpkgs.follows = "nixpkgs";

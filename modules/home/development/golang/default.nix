9f5290a4583735c27dc76d0b96688a85a72d7a7f
Alexander Wolff <wolffaxn@gmail.com>
2024-07-21 15:05:36 +0200

    feat(hm): configure golang

---
+++ modules/home/development/golang/default.nix
@@ -0,0 +1,39 @@
+{  config, lib, namespace, pkgs, ... }:
+
+with lib;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.development.golang;
+in
+{
+  options.${namespace}.development.golang = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Whether to enable golang";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.go = {
+      enable = true;
+      goPrivate = [
+        "golang.org/x/lint/golint"
+        "golang.org/x/tools/cmd/cover"
+        "golang.org/x/tools/cmd/eg"
+        "golang.org/x/tools/cmd/godoc"
+        "golang.org/x/tools/cmd/goimports"
+        "golang.org/x/tools/cmd/gorename"
+        "golang.org/x/tools/cmd/gotype"
+        "golang.org/x/tools/cmd/guru"
+      ];
+    };
+
+    home.packages = with pkgs; [
+      # required by vscode-go
+      delve
+      # langserver
+      gopls
+    ];
+  };
+}
